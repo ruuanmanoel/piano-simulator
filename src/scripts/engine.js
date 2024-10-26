08456afb6d833f41bbdea8a8ab305b77dd0573a2
Ruan Manoel <ruuanmanoel@gmail.com>
2024-10-26 19:26:46 -0300

    ✨ feat: esconde e mostra letra das teclas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 const volumeSlider = document.querySelector('.volume-slider input');
+const keysCheck = document.querySelector('.keys-check input') ;
 
 let mapedKeys = [];
 let audio = new Audio('./src/tunes/a.wav');
@@ -26,4 +27,10 @@ document.addEventListener('keydown',(e)=>{
 const handleVolume = (e)=>{
     audio.volume = e.target.value;
 }
-volumeSlider.addEventListener('input', handleVolume)
+const showHideKeys = (e)=>{
+    pianoKeys.forEach(key=>{
+        key.classList.toggle('hide')
+    })
+}
+volumeSlider.addEventListener('input', handleVolume);
+keysCheck.addEventListener('click', showHideKeys)
